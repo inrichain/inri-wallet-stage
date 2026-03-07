93e1d449457a53d7fda9bc354b59af29139841a1
INRI CHAIN <contact@inri.life>
2026-03-07 18:50:19 -0300

    Add files via upload

---
+++ src/lib/inri.ts
@@ -0,0 +1,9 @@
+export const INRI = {
+  chainName: "INRI CHAIN",
+  chainIdDec: 3777,
+  rpcUrls: ["https://rpc.inri.life"],
+  blockExplorerUrls: ["https://explorer.inri.life"],
+
+  // Blockscout API v2 (mais comum)
+  explorerApiBase: "https://explorer.inri.life/api/v2",
+};
