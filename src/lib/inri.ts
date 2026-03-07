03438f8ce68c75d5ccc2117dfb637c7fd99c970f
INRI CHAIN <contact@inri.life>
2026-03-07 18:54:08 -0300

    Delete src/lib/inri.ts

--- src/lib/inri.ts
+++
@@ -1,9 +0,0 @@
-export const INRI = {
-  chainName: "INRI CHAIN",
-  chainIdDec: 3777,
-  rpcUrls: ["https://rpc.inri.life"],
-  blockExplorerUrls: ["https://explorer.inri.life"],
-
-  // Blockscout API v2 (mais comum)
-  explorerApiBase: "https://explorer.inri.life/api/v2",
-};
